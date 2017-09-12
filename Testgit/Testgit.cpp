ae32ed56aa8544c04084f06ca223975c36c27f01
NikitaKrukovich <mr.niki1999@mail.ru>
2017-09-12 08:19:24 +0300

    first commit

---
+++ Testgit/Testgit.cpp
@@ -0,0 +1,14 @@
+// Testgit.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x = 10;
+	char f = 'f';
+	cout << "Git." << endl;
+    return 0;
+}
+
